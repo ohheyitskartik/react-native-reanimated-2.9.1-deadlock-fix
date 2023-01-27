5646a0b1961d7a780cc57d3fda103220ed97ace2
Kartik Rana <kartikrana@M-RWHFXY9YYG.local>
2023-01-27 15:06:15 +0530

    final compiled package

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  modulePathIgnorePatterns: ['Example', 'docs', 'lib'],
-  setupFiles: ['./jest-setup.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-};
